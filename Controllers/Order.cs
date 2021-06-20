73f08baad3bbc14e528eed6226754abdc894cc36
John Castle <jecastle4@gmail.com>
2021-06-20 11:01:27 +0100

    Order and Parcel

---
+++ Controllers/Order.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace FundApps.Controllers
+{
+    public class Order
+    {
+        public IList<Parcel> Parcels { get; set; }
+        public double Cost 
+        {
+            get
+            {
+                double orderCost = 0;
+                foreach(Parcel parcel in Parcels)
+                {
+                    orderCost += parcel.Cost;
+                }
+                return orderCost;
+            }
+        }
+    }
+}
---
+++ Controllers/Parcel.cs
@@ -0,0 +1,17 @@
+﻿namespace FundApps.Controllers
+{
+    public class Parcel
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+        public double Cost 
+        { 
+            get
+            {
+                FixedDeliveryCostCalculator costCalculator = new FixedDeliveryCostCalculator();
+                return costCalculator.GetCost(Length, Width, Depth);
+            } 
+        }
+    }
+}
