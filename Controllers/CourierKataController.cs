9d5667f49154d72723ebad68388ff185db637f4f
John Castle <jecastle4@gmail.com>
2021-06-20 09:57:53 +0100

    Add CourierKata

---
+++ Controllers/CourierKataController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FundApps.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourierKataController : ControllerBase
+    {
+        [HttpGet]
+        public double Get(double length, double width, double depth)
+        {
+            return 0;
+        }
+    }
+}
---
+++ CourierKata.cs
@@ -0,0 +1,9 @@
+﻿namespace FundApps
+{
+    public class CourierKata
+    {
+        public double Length { get; set; }
+        public double Width { get; set; }
+        public double Deptth { get; set; }
+    }
+}
